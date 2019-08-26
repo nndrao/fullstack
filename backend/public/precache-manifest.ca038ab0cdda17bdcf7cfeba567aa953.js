4c2afbb33552b0202cf8597785aea12733987198
Anand Rao <andyrao@Anands-iMac-Pro.local>
2019-08-25 20:25:56 -0400

    initial commit

---
+++ backend/public/precache-manifest.ca038ab0cdda17bdcf7cfeba567aa953.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60ce1e078b653efee25339ee3a2314b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df471a5cd6e98a20ca91",
+    "url": "/static/css/main.2d7fbd60.chunk.css"
+  },
+  {
+    "revision": "021cae8fc321f06c8b72",
+    "url": "/static/js/2.11d2f257.chunk.js"
+  },
+  {
+    "revision": "df471a5cd6e98a20ca91",
+    "url": "/static/js/main.ab579e04.chunk.js"
+  },
+  {
+    "revision": "db6be01aac7cb1c6d4ff",
+    "url": "/static/js/runtime~main.0033641b.js"
+  }
+]);
